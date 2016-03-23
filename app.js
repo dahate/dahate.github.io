d30dd63b84da020b6882f5c9d64262bd5c6795f4
dahate <mkretsch4@web.de>
2016-03-23 19:20:24 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var config = {
   // - Your app's id on moneypot.com
   app_id: 1031,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
-  app_name: 'BTC-Dice + Free BTC',
+  app_name: 'https://dahate.github.io',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6Lf6xBgTAAAAAO3Gx1AVGETcfL2RBah2VAw8GCjM',  // <----- EDIT ME!
